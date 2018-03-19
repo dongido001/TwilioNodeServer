d59513a3ca4db7e5c0e3a0a3247effffc47a680f
Onwuka Gideon <dongidomed@gmail.com>
2018-03-20 00:27:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Twilio Node.js app for generating access token demo.
+
+## How to run
+
+- Install all packages:
+```
+     npm install
+```
+- Create your configuration file and update .env file with your Twilio: Token
+```
+   cp .env.example .env
+```
+- Run the app
+```
+  npm start
+```
+
+## Example:
+
+ - http://localhost:3002/token?identity=dongido
+
+## Note:
+
+This app is not secured! This means any user can use it to generate token. If you intend to use this in real world, make sure to add authentication.
---
+++ server.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+ * Load Twilio configuration from .env config file - the following environment
+ * variables should be set:
+ * process.env.TWILIO_ACCOUNT_SID
+ * process.env.TWILIO_API_KEY
+ * process.env.TWILIO_API_SECRET
+ */
+
+require('dotenv').load();
+
+var http = require('http');
+var AccessToken = require('twilio').jwt.AccessToken;
+var VideoGrant = AccessToken.VideoGrant;
+var express = require('express');
+
+// Create Express webapp.
+var app = express();
+
+/**
+ * Generate an Access Token for a chat application user provided via the url
+ */
+app.get('/token', function(request, response) {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ 
+  var identity = request.query['identity'];
+
+  if ( !identity ) {
+    response.send({
+      body: "An identity is needed"
+    })
+  }
+
+  // Create an access token which we will sign and return to the client,
+  // containing the grant we just created.
+  var token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET
+  );
+
+  // Assign the generated identity to the token.
+  token.identity = identity;
+
+  // Grant the access token Twilio Video capabilities.
+  var grant = new VideoGrant();
+  token.addGrant(grant);
+
+  // Serialize the token to a JWT string and include it in a JSON response.
+  response.send({
+    identity: identity,
+    token: token.toJwt()
+  });
+});
+
+// Create http server and run it.
+var server = http.createServer(app);
+var port = process.env.PORT || 3002;
+server.listen(port, function() {
+  console.log('Express server running on *:' + port);
+});
